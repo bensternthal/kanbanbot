118256c1885688a4e68f0b6550897eedb7bec58e
Benjamin Sternthal <ben@devpatch.com>
2013-05-17 11:42:09 -0700

    testing

--- app.js
+++ app.js
@@ -13,45 +13,46 @@ var kanbanery = require('./lib/kanbanery');
 nconf.argv().env().file({ file: 'local.json' });
 
 // IRC Client
-// var client = new irc.Client(nconf.get('irc:server'), nconf.get('irc:botName'), nconf.get('irc:config'));
+//var client = new irc.Client(nconf.get('irc:server'), nconf.get('irc:botName'), nconf.get('irc:config'));
 
-// client.addListener('error', function(message) {
-//     console.log('error: ', message);
-// });
+//client.addListener('error', function(message) {
+//    console.log('error: ', message);
+//});
 
 
-// http.createServer(function (req, res) {
-//   var queryObject = url.parse(req.url,true).query;
-//   res.writeHead(200, {"Content-Type": "text/plain"});
-//   res.end();
-//   //check if this blocks
-//   processResponse(queryObject);
-// }).listen(8000);
+http.createServer(function (req, res) {
+
+  //authenticate
+  var queryObject = url.parse(req.url,true).query;
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end('I am here\n');
+  //get id from liveupdate url
+  console.log(queryObject);
+  //processResponse(queryObject);
+}).listen(8000);
 
 // // Async much?
-// var processResponse = function(queryObject) {
+var processResponse = function(queryObject) {
 
-//   kanbanery.queryCard(queryObject, function(err, response){
-//     if (err || !response) {
-//       console.log('something shitty:' + err);
-//     } else {
-//       //lets look up column
-//       var cardData = response;
-//       kanbanery.queryColumn(response, function(err, response){
-//         postIRC(cardData, response);
-//       })
-//     }
-//   });  
-// }
+// request { "user_id" => "7", "auth_token" => "4bd1ffc83e5ed9ac501c6d1729c8643c9022d452", "resource" => { "id" => "6", "title" => "Some task", ... } }
+  kanbanery.get(883227, 'tasks', nconf, function(err, response){
+    if (err || !response) {
+      console.log('something shitty:' + err);
+    } else {
+      //lets look up column
+      var cardData = response;
+      console.log(response);
+      //kanbanery.queryColumn(response, function(err, response){
+        //postIRC(cardData, response);
+      //})
+    }
+  });  
+}
 
-// var postIRC = function(cardData, columnData) {
-//   var message = "Ben Test - Card: '" + cardData.title + "' was moved to: " + columnData.name;
-//   client.say("#firefoxflicks", message);
-// }
-
-// client.addListener("message", function(from, to, text, message) {
-//   client.say("#bentest", "¿Public que?");
-// });
+var postIRC = function(cardData, columnData) {
+  var message = "Ben Test - Card: '" + cardData.title + "' was moved to: " + columnData.name;
+  client.say("#firefoxflicks", message);
+}
 
 // todo!
 // get live update request
